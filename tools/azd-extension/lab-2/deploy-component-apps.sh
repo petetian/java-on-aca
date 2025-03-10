fa60784a0751e40866a7e69f28f5e522b293f058
Pete Tian <pete.tian@gmail.com>
2025-03-10 14:57:41 -0700

    create docker images for microservices

--- tools/azd-extension/lab-2/deploy-component-apps.sh
+++ tools/azd-extension/lab-2/deploy-component-apps.sh
@@ -4,3 +4,19 @@
 cd ../../../spring-petclinic-microservices
 git submodule update --init
 mvn clean package -DskipTests
+
+# Build the Docker image using the Dockerfile
+
+DOCKERFILE="../tools/Dockerfile"
+if [ ! -f $DOCKERFILE ]; then
+    echo "Dockerfile not found at $DOCKERFILE"
+    exit 1
+fi
+
+docker build -t petclinic-admin-server:latest -f $DOCKERFILE ./spring-petclinic-admin-server
+docker build -t petclinic-api-gateway:latest -f $DOCKERFILE ./spring-petclinic-api-gateway
+docker build -t petclinic-config-server:latest -f $DOCKERFILE ./spring-petclinic-config-server
+docker build -t petclinic-customers-service:latest -f $DOCKERFILE ./spring-petclinic-customers-service
+docker build -t petclinic-discovery-server:latest -f $DOCKERFILE ./spring-petclinic-discovery-server
+docker build -t petclinic-vets-service:latest -f $DOCKERFILE ./spring-petclinic-vets-service
+docker build -t petclinic-visits-service:latest -f $DOCKERFILE ./spring-petclinic-visits-service
