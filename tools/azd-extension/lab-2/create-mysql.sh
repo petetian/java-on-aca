4f8ab79515466c7521d5e4493b18c712a21e9a8e
Pete Tian <pete.tian@gmail.com>
2025-03-11 13:32:02 -0700

    Refactor: source the config file created by the previous step. Add application config yaml

--- tools/azd-extension/lab-2/create-mysql.sh
+++ tools/azd-extension/lab-2/create-mysql.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-source ./azure-resource.profile
+AZURE_CONFIG_FILE="../config/azure-resource.profile"
+source $AZURE_CONFIG_FILE
+
+APPLICATION_CONFIG_FILE="../config/application-mysql.yml"
 
 # This script creates an Azure MySQL server and an Azure Container Registry (ACR) in a specified resource group.
 MYSQL_SERVER_NAME=mysql-$APPNAME-$UNIQUEID
 MYSQL_ADMIN_USERNAME=sqladmin
-MYSQL_ADMIN_PASSWORD="5qVYxsoyaV9qJN"
+MYSQL_ADMIN_PASSWORD=$(openssl rand -hex 12)
 DATABASE_NAME=petclinic
 
 az mysql flexible-server create \
@@ -28,3 +31,28 @@ az mysql flexible-server firewall-rule create \
     --name $MYSQL_SERVER_NAME \
     --resource-group $RESOURCE_GROUP \
     --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
+
+# Set up a configuration repository
+# set up the application configuration settings that allow Spring Boot applications to connect with the database.
+echo "
+spring:
+    datasource:
+        url: jdbc:mysql://${MYSQL_SERVER_NAME}.mysql.database.azure.com:3306/petclinic?useSSL=true
+        username: ${MYSQL_ADMIN_USERNAME}
+        password: ${MYSQL_ADMIN_PASSWORD}
+    sql:
+        init:
+            schema-locations: classpath*:db/mysql/schema.sql
+            data-locations: classpath*:db/mysql/data.sql
+            mode: ALWAYS
+" > $APPLICATION_CONFIG_FILE
+
+export GIT_URI="https://github.com/Azure-Samples/java-on-aca.git"
+export SEARCH_PATH="config"
+export LABEL=main
+
+{
+    echo "GIT_URI=https://github.com/Azure-Samples/java-on-aca.git"
+    echo "SEARCH_PATH=config"
+    echo "LABEL=main"
+} >> $AZURE_CONFIG_FILE
