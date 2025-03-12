766ca0a86dd624e58d76f69b18375379aa4c6b8e
Pete Tian <pete.tian@gmail.com>
2025-03-11 17:38:55 -0700

    add new module

--- tools/azd-extension/lab-2/deploy-component-apps.sh
+++ tools/azd-extension/lab-2/deploy-component-apps.sh
@@ -1,22 +1,55 @@
 #!/bin/bash
 
+AZURE_CONFIG_FILE="../config/azure-resource.profile"
+source $AZURE_CONFIG_FILE
+
 # Get the application code from public upstream repo, and then build the applications.
 cd ../../../spring-petclinic-microservices
 git submodule update --init
 mvn clean package -DskipTests
 
 # Build the Docker image using the Dockerfile
-
 DOCKERFILE="../tools/Dockerfile"
 if [ ! -f $DOCKERFILE ]; then
-    echo "Dockerfile not found at $DOCKERFILE"
+    echo "$DOCKERFILE not found "
     exit 1
 fi
 
-docker build -t petclinic-admin-server:latest -f $DOCKERFILE ./spring-petclinic-admin-server
-docker build -t petclinic-api-gateway:latest -f $DOCKERFILE ./spring-petclinic-api-gateway
-docker build -t petclinic-config-server:latest -f $DOCKERFILE ./spring-petclinic-config-server
-docker build -t petclinic-customers-service:latest -f $DOCKERFILE ./spring-petclinic-customers-service
-docker build -t petclinic-discovery-server:latest -f $DOCKERFILE ./spring-petclinic-discovery-server
-docker build -t petclinic-vets-service:latest -f $DOCKERFILE ./spring-petclinic-vets-service
-docker build -t petclinic-visits-service:latest -f $DOCKERFILE ./spring-petclinic-visits-service
+# if ACA environment does not exist, exit
+if [ -z "$ACA_ENVIRONMENT" ]; then
+    echo "Error: ACA environment [$ACA_ENVIRONMENT] not found."
+    exit 1
+fi
+
+APP_NAME="api-gateway"
+cp -f ../tools/Dockerfile ./spring-petclinic-$APP_NAME/Dockerfile
+
+# Disable path conversion for MSYS (Git Bash)
+export MSYS_NO_PATHCONV=1
+
+echo "az containerapp create \
+    --name $APP_NAME \
+    --resource-group $RESOURCE_GROUP \
+    --environment $ACA_ENVIRONMENT \
+    --source ./spring-petclinic-$APP_NAME \
+    --registry-server $MYACR.azurecr.io \
+    --registry-identity $APPS_IDENTITY_ID \
+    --ingress external \
+    --target-port 8080 \
+    --min-replicas 1 \
+    --bind $JAVA_CONFIG_COMP_NAME $JAVA_EUREKA_COMP_NAME \
+    --runtime java"
+
+az containerapp create \
+    --name $APP_NAME \
+    --resource-group $RESOURCE_GROUP \
+    --environment $ACA_ENVIRONMENT \
+    --source ./spring-petclinic-$APP_NAME \
+    --registry-server $MYACR.azurecr.io \
+    --registry-identity $APPS_IDENTITY_ID \
+    --ingress external \
+    --target-port 8080 \
+    --min-replicas 1 \
+    --bind $JAVA_CONFIG_COMP_NAME $JAVA_EUREKA_COMP_NAME \
+    --runtime java
+
