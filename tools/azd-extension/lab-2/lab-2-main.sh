fa209e05e2942f8971dc0489f0ab26347b872db6
Pete Tian <pete.tian@gmail.com>
2025-03-13 10:48:18 -0700

    bug fixing

--- tools/azd-extension/lab-2/lab-2-main.sh
+++ tools/azd-extension/lab-2/lab-2-main.sh
@@ -2,7 +2,7 @@
 
 ./collect-user-info.sh
 ./create-aca-env.sh
-# ./create-mysql.sh
-# ./create-container-registry.sh
-# ./create-java-components.sh
-# ./deploy-component-apps.sh
+./create-mysql.sh
+./create-container-registry.sh
+./create-java-components.sh
+./deploy-component-apps.sh
