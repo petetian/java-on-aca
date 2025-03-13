5c65673dedd9b3cb50079e23b014ba5479bd5b0a
Pete Tian <pete.tian@gmail.com>
2025-03-13 12:44:26 -0700

    clean up

--- tools/azd-extension/lab-2/deploy-component-apps.sh
+++ tools/azd-extension/lab-2/deploy-component-apps.sh
@@ -3,6 +3,15 @@
 AZURE_CONFIG_FILE="../config/azure-resource.profile"
 source $AZURE_CONFIG_FILE
 
+SPRING_PETCLINIC_MICROSERIVCES="../../../spring-petclinic-microservices"
+
+# Check if the spring-petclinic-microservices directory exists
+if [ ! -d "$SPRING_PETCLINIC_MICROSERIVCES" ]; then
+    echo "Error: $SPRING_PETCLINIC_MICROSERIVCES directory does not exist."
+    # create the directory
+    mkdir -p $SPRING_PETCLINIC_MICROSERIVCES
+fi
+
 # Get the application code from public upstream repo, and then build the applications.
 cd ../../../spring-petclinic-microservices
 git submodule update --init
