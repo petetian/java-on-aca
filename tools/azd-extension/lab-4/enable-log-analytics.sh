55f730de2189f5a5ec87c88fdd72767743c4a96d
Pete Tian <pete.tian@gmail.com>
2025-03-19 12:28:46 -0700

    create scripts in lab-4

---
+++ tools/azd-extension/lab-4/enable-log-analytics.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+AZURE_CONFIG_FILE="../config/azure-resource.profile"
+source $AZURE_CONFIG_FILE
+
+WORKSPACE=la-$APPNAME-$UNIQUEID
+az monitor log-analytics workspace create \
+    --resource-group $RESOURCE_GROUP \
+    --workspace-name $WORKSPACE
+
+WORKSPACE_ID=$(az monitor log-analytics workspace show -n $WORKSPACE -g $RESOURCE_GROUP --query id -o tsv)
+WORKSPACE_CUSTOMER_ID=$(az monitor log-analytics workspace show -n $WORKSPACE -g $RESOURCE_GROUP --query customerId -o tsv)
+WORKSPACE_KEY=$(az monitor log-analytics workspace get-shared-keys -n $WORKSPACE -g $RESOURCE_GROUP --query primarySharedKey -o tsv)
+
+az containerapp env update \
+    --name $ACA_ENVIRONMENT \
+    --resource-group $RESOURCE_GROUP \
+    --logs-destination log-analytics \
+    --logs-workspace-id $WORKSPACE_CUSTOMER_ID \
+    --logs-workspace-key $WORKSPACE_KEY
